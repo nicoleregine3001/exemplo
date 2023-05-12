7200657a79714d91e169700af08762b32702ed9e
nicoleregine3001 <nicole_regine@hotmail.com>
2023-05-12 11:21:37 -0300

    novo codigo

---
+++ ex003.py
@@ -0,0 +1,6 @@
+# Soma entre dois números com interação do usuário.
+
+numero1 = int(input('Digite um número:'))
+numero2 = int(input('Digite outro número:'))
+soma = numero1 + numero2
+print('A soma é:', soma)
